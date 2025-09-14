b3af7010fa71bfaeed93cc8ffa3e27bce7e164a3
MarcusWentz <markzcollege404@gmail.com>
2025-09-14 17:52:04 -0400

    lnInput ratio

--- src/Counter.sol
+++ src/Counter.sol
@@ -15,11 +15,18 @@ contract Counter {
     // sd = input is already scaled by multiplying by 1 ether
     // convert = unscaled input that will be scalled multiplying value by 1 ether
     
-    SD59x18 a = sd(-1 ether);
+    SD59x18 xReserve = sd(6 ether);
     SD59x18 yReserve = sd(0 ether);
-    SD59x18 lnInput = sd(0.4444 ether);
+    SD59x18 Dx = sd(2 ether);
+    SD59x18 a = sd(-1 ether);
+    SD59x18 c = sd(9 ether);
+    SD59x18 b = sd(0 ether);
+    SD59x18 lnInputDenominator = c - b;
+    SD59x18 lnInputNumerator = (xReserve - Dx ) - b;
+    SD59x18 lnInput = lnInputNumerator.div(lnInputDenominator);
+    // SD59x18 lnInput = sd(0.444444444444444444 ether);
     SD59x18 lnExpression = lnInput.ln();
-    // SD59x18 lnExpression = convert(-0.8 ether);
+    // SD59x18 lnExpression = sd(-0.810930216216328753 ether);
     SD59x18 lnExpressionScaled = lnExpression.mul(a);
     SD59x18 resultWrapped = lnExpressionScaled - yReserve;
     result = SD59x18.unwrap(resultWrapped);
